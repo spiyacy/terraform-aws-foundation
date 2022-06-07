9d2218ebbc0575d9fe532bdb47d0a286f95d53f9
Brandon <brandon.miller@hashicorp.com>
2022-06-07 13:33:15 -0400

    SSH Access
    Public accessibility to SSH over port 22

--- main.tf
+++ main.tf
@@ -51,6 +51,13 @@ resource "aws_security_group" "sg_ingress" {
     protocol    = "-1"
     cidr_blocks = [module.vpc.vpc_cidr_block]
   }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_security_group" "sg_egress" {
