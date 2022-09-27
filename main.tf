9c7107ac93ada1bece9b0b1eb6f9f52d237ef89b
Brandon <22477738+spiyacy@users.noreply.github.com>
2022-09-27 10:34:41 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,14 +43,6 @@ resource "aws_security_group" "sg_ingress" {
   name        = "${var.prefix}_foundation_ingress_sg"
   description = "${var.prefix} Foundational Ingress Security Group"
   vpc_id      = module.vpc.vpc_id
-
-  # vpc cidr block
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
 resource "aws_security_group" "sg_egress" {
